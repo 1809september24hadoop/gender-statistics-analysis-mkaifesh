8488f904dd21b82b50e1557335377d8110323c26
Mike Kaifesh <mkaifesh@kent.edu>
2018-10-24 18:36:25 -0700

    Adding Question 4/5 mapper reducer

---
+++ Project1/src/main/java/com/revature/map/WorldMEmploymentMapper.java
@@ -0,0 +1,26 @@
+package com.revature.map;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class WorldMEmploymentMapper extends Mapper<LongWritable,Text,Text,DoubleWritable>{
+	public void map(LongWritable key, Text value, Context context)throws IOException, InterruptedException{
+		String line = value.toString();
+		String trimedLine = line.substring(1, line.length() - 1);
+		String[] parts = trimedLine.split("\",\"");
+		parts[60] = parts[60].replaceAll("\"", "");
+		String country = parts[0];
+		if(parts[3].equals("SL.EMP.TOTL.SP.MA.ZS")){
+			for(int i = 44; i < parts.length; i++){
+				String yearData = parts[i];
+				if(!yearData.equals("")){
+					context.write(new Text(country), new DoubleWritable(Double.parseDouble(yearData)));
+				}
+			}
+		}
+	}
+}
---
+++ Project1/src/main/java/com/revature/reduce/WorldMEmploymentReducer.java
@@ -0,0 +1,28 @@
+package com.revature.reduce;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import com.google.common.util.concurrent.AtomicDouble;
+
+public class WorldMEmploymentReducer extends Reducer<Text,DoubleWritable,Text,DoubleWritable>{
+	static AtomicDouble firstPercentValue = new AtomicDouble(0.0);
+	static AtomicDouble runningIncrease = new AtomicDouble(0.0);
+	public void reduce(Text key, Iterable<DoubleWritable> values, Context context)throws IOException, InterruptedException{
+		boolean firstValue = true;
+		for(DoubleWritable value: values){
+			if(firstValue){
+				firstValue = false;
+				firstPercentValue = new AtomicDouble(value.get());
+			}
+			else{
+				runningIncrease = new AtomicDouble(value.get()-runningIncrease.get());
+			}
+		}
+		context.write(new Text("Increase in male employment percentage since 2000: "+key), new DoubleWritable(-1*runningIncrease.get()));
+	}
+
+}
